9babf68e211efb67d03f823c7605430a4e8692b7
Brayden <Braydenlwong@gmail.com>
2023-02-25 21:36:41 -0700

    updated projects background to fit theme

--- src/features/projects.tsx
+++ src/features/projects.tsx
@@ -11,8 +11,8 @@ export const Projects = () => {
       <div className="relative flex w-full flex-row overflow-x-scroll">
         {PROJECTS.map((project) => (
           <div
-            className="flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20
-            md:p-44"
+            className="flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 
+            p-20 md:p-44"
           >
             <div className="max-w-6xl space-y-10 px-0 text-white md:px-10">
               <h4 className="text-center text-4xl underline decoration-teal-400">
@@ -25,6 +25,8 @@ export const Projects = () => {
           </div>
         ))}
       </div>
+      <div className="absolute top-1/4 left-0 z-20 h-[600px] w-full skew-y-12 bg-teal-400/20" />
+      <div className="absolute left-0 bottom-1/3 z-10 h-[250px] w-full -skew-y-[18deg] bg-white/40" />
     </div>
   );
 };
